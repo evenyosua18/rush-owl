54a69eebcbbd4de27ba17b6d28eb2926e29d6be5
evenyosua18 <evenyosua18@gmail.com>
2024-03-11 15:57:16 +0700

    tidy up q2 & q3

--- q3/main.go
+++ q3/main.go
@@ -5,6 +5,7 @@ import (
 	"time"
 )
 
+// worker model to define total worker and store task
 type worker struct {
 	TotalWorker int
 	QueuedTaskC chan func()
@@ -27,26 +28,32 @@ func (w *worker) AddTask(task func()) {
 	w.QueuedTaskC <- task
 }
 
+// Result model to store result of calculation rectangle's area and to passed the data via channel
 type Result struct {
 	TaskID int
 	Side   float64
 	Area   float64
 }
 
+// CalculateRectangleArea function to calculate area of rectangle and give 3 seconds buffer
 func CalculateRectangleArea(side float64) float64 {
 	time.Sleep(3 * time.Second)
 	return side * side
 }
 
 func main() {
+	// initiate and run worker
 	w := worker{TotalWorker: 3, QueuedTaskC: make(chan func())}
 	w.Run()
 
+	// save start time just for a record
 	startTime := time.Now()
 
+	// define total task
 	totalTask := 9
 	resultC := make(chan Result, totalTask)
 
+	// assign task
 	for i := 0; i < totalTask; i++ {
 		taskID := i + 1
 		side := float64(i) + 0.5
@@ -56,10 +63,12 @@ func main() {
 		})
 	}
 
+	// get result of task
 	for i := 0; i < totalTask; i++ {
 		res := <-resultC
 		log.Printf("task id %d and side %.1f finisihed with result %.2f", res.TaskID, res.Side, res.Area)
 	}
 
+	// just to record the total execution time
 	log.Printf("finish all tasks with %f seconds\n", time.Now().Sub(startTime).Seconds())
 }
