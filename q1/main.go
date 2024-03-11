611e4f93c8f063d15616a14e367b3a58c0665c2c
evenyosua18 <evenyosua18@gmail.com>
2024-03-11 15:51:38 +0700

    give more explanation for q1

--- q1/main.go
+++ q1/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 )
 
+// Rectangle struct method without pointer
 type Rectangle struct {
 	Side float32
 }
@@ -16,6 +17,7 @@ func (r Rectangle) SetSide(side float32) {
 	r.Side = side
 }
 
+// Circle struct method with pointer
 type Circle struct {
 	Radius float32
 }
@@ -28,32 +30,36 @@ func (c *Circle) SetRadius(radius float32) {
 	c.Radius = radius
 }
 
-func AddSide(r *Rectangle) {
-	r.Side += 5
+// AddSide function to update side of rectangle, request rectangle model with pointer
+func AddSide(r *Rectangle, add float32) {
+	r.Side += add
 }
 
-func AddRadius(c Circle) {
-	c.Radius += 5
+// AddRadius function to update radius of circle, request circle model without pointer
+func AddRadius(c Circle, add float32) {
+	c.Radius += add
 }
 
 func main() {
+	// initiate rectangle
 	rect := Rectangle{Side: 2.5}
 
-	fmt.Println(rect.GetArea())
-	rect.SetSide(4.5)
-	fmt.Println(rect.GetArea())
-
+	// initiate circle
 	circle := Circle{5}
 
-	fmt.Println(circle.GetArea())
+	fmt.Println("area of rectangle before side edited:", rect.GetArea())
+	rect.SetSide(4.5)
+	fmt.Println("area of rectangle after side edited:", rect.GetArea())
+
+	fmt.Println("area of circle before radius edited:", circle.GetArea())
 	circle.SetRadius(10)
-	fmt.Println(circle.GetArea())
+	fmt.Println("area of circle after radius edited", circle.GetArea())
 
-	fmt.Println(rect.Side)
-	AddSide(&rect)
-	fmt.Println(rect.Side)
+	fmt.Println("side of rectangle before added", rect.Side)
+	AddSide(&rect, 5)
+	fmt.Println("side of rectangle after added", rect.Side)
 
-	fmt.Println(circle.Radius)
-	AddRadius(circle)
-	fmt.Println(circle.Radius)
+	fmt.Println("radius of circle before added", circle.Radius)
+	AddRadius(circle, 5)
+	fmt.Println("radius of circle after added", circle.Radius)
 }
